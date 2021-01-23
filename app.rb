8a84559b14a1cee0e18dbb7e1d110f55bf1440b2
Sierra Wallace <sskwallace@gmail.com>
2021-01-23 23:40:03 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -10,8 +10,12 @@ class App < Sinatra::Base
     erb :reversed
   end
 
-  get '/friends' do
-    # Write your code here!
+  
+    get '/friends' do
+  @friends = ['Emily Wilding Davison', 'Harriet Tubman', 'Joan of Arc', 'Malala Yousafzai', 'Sojourner Truth']
+ 
+  erb :friends
+end
 
   end
 end
